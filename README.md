7450867fd3ffa6940ae7b2b3a539f7bfae3a8085
Christopher Moore <christopher.e.moore@gmail.com>
2017-01-30 14:56:14 +0530

    making things look better

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Topple matrices from [numberphile](https://www.youtube.com/watch?v=1MtEUErz7Gg)
 
+![](images/1048576.png)
+
 CPU version flags and updates only dirty cells.
 
 GPU version processes all at once.
--- plot.lua
+++ plot.lua
@@ -10,7 +10,9 @@ require 'gnuplot'{
 	output = 'results.png',
 	style = 'data linespoints',
 	log = 'xy',
-	title = 'cpu vs gpu on a '..gridsize..' grid',
+	title = 'Performance',
+	xlabel = 'initial stack size',
+	ylabel = 'time to compute (seconds)',
 	{datafile='results.txt', using='1:2', title='CPU'},
 	{datafile='results.txt', using='1:3', title='GPU'},
 }
